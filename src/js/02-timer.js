20a277a6bb0436f2ced0c708bcd95380bcb5be2e
garymiller <77537560+repro-tetris@users.noreply.github.com>
2021-07-11 13:07:50 +0300

    02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,8 @@
-//import '../css/common.css';
+import '../css/common.css';
 import '../css/02-timer.scss';
 import Swal from 'sweetalert2';
+
+const TIMER_INTERVAL = 1000;
 
 const refs = {
   startCountdown: document.querySelector('button[data-start]'),
@@ -10,22 +12,63 @@ const refs = {
   minutesEl: document.querySelector('span[data-minutes]'),
   secondsEl: document.querySelector('span[data-seconds]'),
 };
+let enteredDate = null;
+let intervalId = null;
 
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
+refs.dateSelectorEl.addEventListener('change', function (e) {
+  enteredDate = new Date(e.target.value).getTime();
+  if (enteredDate - Date.now() <= 0) {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Please choose a date in the future',
+      icon: 'error',
+      confirmButtonText: 'Ok',
+    });
+    return;
+  }
 
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  refs.startCountdown.disabled = false;
 
-//   return { days, hours, minutes, seconds };
-// }
+  console.log(enteredDate);
+});
+
+refs.startCountdown.addEventListener('click', () => {
+  refs.dateSelectorEl.disabled = true;
+  refs.startCountdown.disabled = true;
+  enteredDate = Date.now() + 5 * TIMER_INTERVAL;
+
+  intervalId = setInterval(function () {
+    const curInterval = enteredDate - Date.now();
+
+    const { days, hours, minutes, seconds } = convertMs(curInterval);
+
+    refs.daysEl.innerHTML = days.toString().padStart(3, '0');
+    refs.hoursEl.innerHTML = hours.toString().padStart(2, '0');
+    refs.minutesEl.innerHTML = minutes.toString().padStart(2, '0');
+    refs.secondsEl.innerHTML = seconds.toString().padStart(2, '0');
+
+    if (days + hours + minutes + seconds === 0) {
+      clearInterval(intervalId);
+      refs.dateSelectorEl.disabled = false;
+    }
+  }, TIMER_INTERVAL);
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
