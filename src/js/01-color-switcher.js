e86e77d23329f1e1873e8a016710eb571d217573
garymiller <77537560+repro-tetris@users.noreply.github.com>
2021-07-11 11:56:26 +0300

    check css loader

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
-import '../css/common.css';
+//import '../css/common.css';
+
+const TIMER_INTERVAL = 1000;
+
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+let timerId = null;
+
+refs.btnStart.addEventListener('click', startTimer);
+refs.btnStop.addEventListener('click', stopTimer);
+
+function startTimer() {
+  refs.btnStart.disabled = true;
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, TIMER_INTERVAL);
+}
+
+function stopTimer() {
+  if (timerId === null) return;
+  clearInterval(timerId);
+  timerId = null;
+  refs.btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,1 +1,31 @@
-import '../css/common.css';
+//import '../css/common.css';
+import '../css/02-timer.scss';
+import Swal from 'sweetalert2';
+
+const refs = {
+  startCountdown: document.querySelector('button[data-start]'),
+  dateSelectorEl: document.querySelector('#date-selector'),
+  daysEl: document.querySelector('span[data-days]'),
+  hoursEl: document.querySelector('span[data-hours]'),
+  minutesEl: document.querySelector('span[data-minutes]'),
+  secondsEl: document.querySelector('span[data-seconds]'),
+};
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
